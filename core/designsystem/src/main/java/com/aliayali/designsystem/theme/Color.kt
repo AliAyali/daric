62c9d394e803857b0aeefe58f9c06848132537df
AliAyali <ali.ayali.programmer@gmail.com>
2026-07-25 18:20:56 +0330

    feat(designsystem): add color

---
+++ core/designsystem/src/main/java/com/aliayali/designsystem/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.aliayali.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val BrandPrimary = Color(0xFFA0E86F)
+internal val BrandSecondary = Color(0xFF154406)
+
+// Semantic
+internal val Success = Color(0xFF12D18E)
+internal val Warning = Color(0xFFFACC15)
+internal val Error = Color(0xFFF75555)
+internal val Info = BrandPrimary
+
+// Disabled
+internal val DisabledLight = Color(0xFFD8D8D8)
+internal val DisabledDark = Color(0xFF23252B)
+internal val DisabledButton = Color(0xFF80BA59)
+
+// Backgrounds
+internal val BackgroundTeal = Color(0xFFF0FBFA)
+internal val BackgroundPurple = Color(0xFFF8F0FE)
+internal val BackgroundRed = Color(0xFFFFF3F3)
+internal val BackgroundBlue = Color(0xFFF3F6FF)
+
+// Common
+internal val White = Color(0xFFFFFFFF)
+internal val Black = Color(0xFF000000)
+internal val Transparent = Color.Transparent
