acb0a1176fc093177b853bfd4e867228b09f2524
AliAyali <ali.ayali.programmer@gmail.com>
2026-07-30 20:29:11 +0330

    feat(home): add loading state UI

--- feature/home/src/main/java/com/aliayali/home/HomeScreen.kt
+++ feature/home/src/main/java/com/aliayali/home/HomeScreen.kt
@@ -1,16 +1,30 @@
 package com.aliayali.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.aliayali.designsystem.component.DaricLoadingIndicator
 
 @Composable
 fun HomeScreen(
     uiState: HomeUiState,
     onEvent: (HomeEvent) -> Unit,
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) { }
+    when (uiState) {
+
+        HomeUiState.Loading -> {
+            DaricLoadingIndicator()
+        }
+
+        is HomeUiState.Success -> {
+
+        }
+
+        is HomeUiState.Error -> {
+
+        }
+    }
 }
