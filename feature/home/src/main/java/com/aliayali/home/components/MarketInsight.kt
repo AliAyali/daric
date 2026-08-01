8a1e28147acfb678777c408b002f132e8fc05b7a
AliAyali <ali.ayali.programmer@gmail.com>
2026-08-01 19:09:18 +0330

    feat(home): add animated MarketInsight component

---
+++ feature/home/src/main/java/com/aliayali/home/components/MarketInsight.kt
@@ -0,0 +1,51 @@
+package com.aliayali.home.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun MarketInsight(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+) {
+
+    Column(
+        modifier = modifier,
+    ) {
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start,
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.White,
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(6.dp)
+        )
+
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start,
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.White.copy(alpha = .65f),
+        )
+
+    }
+
+}
