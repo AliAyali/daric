b1f6111f410170da85f0f0fa61047f8ca89eae08
AliAyali <ali.ayali.programmer@gmail.com>
2026-08-05 17:17:43 +0330

    feat(home): add coin icon mapper

---
+++ feature/home/src/main/java/com/aliayali/home/mapper/coinIcon.kt
@@ -0,0 +1,21 @@
+package com.aliayali.home.mapper
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.CurrencyBitcoin
+import androidx.compose.material.icons.filled.Diamond
+import androidx.compose.material.icons.filled.Euro
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MonetizationOn
+import androidx.compose.ui.graphics.vector.ImageVector
+
+fun coinIcon(symbol: String): ImageVector {
+    return when (symbol.uppercase()) {
+        "BTC" -> Icons.Default.CurrencyBitcoin
+        "ETH" -> Icons.Default.Diamond
+        "USD" -> Icons.Default.AttachMoney
+        "EUR" -> Icons.Default.Euro
+        "XAU", "GOLD" -> Icons.Default.MonetizationOn
+        else -> Icons.Default.AttachMoney
+    }
+}
